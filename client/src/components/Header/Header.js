5854f5e38f3041357aa8c40dbb4571f7659f6757
DESKTOP-P02URSR\John <jmcarroll95@gmail.com>
2018-03-10 01:06:15 -0500

    added logo and updated styling on home page and header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { Navbar } from 'react-materialize';
 import { NavLink } from "react-router-dom";
-
+import Logo from "./logo.png";
 import './Header.css';
+
+const Img = <img src={Logo} alt={"Hotplate"} className={"logo"}/>;
 
 class Header extends Component {
   render() {
     return (
-      <Navbar className="Header red darken-4" brand='Hotplate' right>
-        <li><NavLink to="">About</NavLink></li>
-        <li><NavLink to="login">Log In</NavLink></li>
+      <Navbar className="Header red lighten-5" brand={Img} right>
+        <li><NavLink to="" className="black-text">About</NavLink></li>
+        <li><NavLink to="login" className="black-text">Log In</NavLink></li>
       </Navbar>
     );
   }
