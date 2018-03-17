ead76dbdabb8d918aea5adc62f81ce84f961d4e1
DESKTOP-P02URSR\John <jmcarroll95@gmail.com>
2018-03-17 03:16:05 -0400

    removed background color and gradient from home page. Went with more muted color pallete.

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -9,11 +9,11 @@ import '../../css/full-width.css';
 class About extends Component {
   render() {
     return (
-      <div className="gradient-animated gradient full-width-bar">
+      <div className="about-theme full-width-bar">
         <Row>
           <Col s={12}>
             <span className="white-text">
-              <h2>Build wonderful things</h2>
+              <h2 style={{marginTop:15.00}}>Build wonderful things</h2>
               <div className="text-size-default">
                 hotplate is a platform for building customizable and reusable template code. Find a template, and use it in your project.
               </div>
