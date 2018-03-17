8de22b417e8e80bd548e593f17c6c8ab8f583bb5
DESKTOP-P02URSR\John <jmcarroll95@gmail.com>
2018-03-17 14:22:28 -0400

    added margin to about bottom

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -12,7 +12,7 @@ class About extends Component {
       <div className="about-theme full-width-bar">
         <span className="white-text">
           <h2 style={{marginTop:0.00}}>Build wonderful things</h2>
-          <div className="text-size-default">
+          <div className="text-size-default container-size">
             hotplate is a platform for building customizable and reusable template code. Find a template, and use it in your project.
           </div>
         </span>
