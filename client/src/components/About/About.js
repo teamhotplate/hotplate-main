5a7c8d4741abf7ad3e4b74cb8197f0a81de2889b
DESKTOP-P02URSR\John <jmcarroll95@gmail.com>
2018-03-17 13:33:22 -0400

    changed search results so that it hides, and only appears when you search for something.

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Card, CardTitle } from 'react-materialize';
+// import { Container, Row, Col, Card, CardTitle } from 'react-materialize';
 
 import './About.css';
-import aboutImg from './About.jpg';
+// import aboutImg from './About.jpg';
+
+import '../../css/full-width.css';
 
 class About extends Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <Col s={12}>
-            <Card className="gray" header={<CardTitle image={aboutImg}></CardTitle>}>
-              <span>Hotplate will forever change the landscape of template generation.</span>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <div className="about-theme full-width-bar">
+        <span className="white-text">
+          <h2 style={{marginTop:0.00}}>Build wonderful things</h2>
+          <div className="text-size-default">
+            hotplate is a platform for building customizable and reusable template code. Find a template, and use it in your project.
+          </div>
+        </span>
+      </div>
     );
   }
 }
